e88e4a82f34bfe62d5419a3ff925f2bbd0a10ece
Gabriel Silva <gabriel.s.alves1997@gmail.com>
2023-02-04 14:43:20 -0300

    Service layer, component registration

---
+++ src/main/java/com/icetop/javaspringboot/services/UserService.java
@@ -0,0 +1,27 @@
+package com.icetop.javaspringboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.icetop.javaspringboot.entities.User;
+import com.icetop.javaspringboot.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+		
+}
